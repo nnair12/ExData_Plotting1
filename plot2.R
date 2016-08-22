be137422b6914b555fe23a947256c11fb4410799
nnair12 <nikhilnairca@gmail.com>
2016-08-21 18:16:10 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,17 @@
+# reading table, taking cells within desired data range
+fullData <- read.table(filename, header = TRUE, sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+data <- fullData[fullData$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+# Changing date format
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+# Creating Datetime field
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## making the plot
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+# Saves histogram
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
